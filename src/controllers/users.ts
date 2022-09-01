69b1dd0cb254225eed11d7d2e96f8b559281e0d8
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-09-01 16:58:46 -0300

    Fix patch users bug

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,10 +4,6 @@ import { User } from '../entity/User'
 
 // post
 export const createUser: RequestHandler = async (req, res, next) => {
-  const id = Math.random()
-  const { email, password, isRealtor } = req.body
-  // const newUser: UserModel = { id, email, password, isRealtor }
-  // USERS.push(newUser)
   const user = await myDataSource.getRepository(User).create(req.body)
   const results = await myDataSource.getRepository(User).save(user)
   res.status(201).json({ message: 'Created new user.', results })
@@ -16,7 +12,7 @@ export const createUser: RequestHandler = async (req, res, next) => {
 // get
 export const getUsers: RequestHandler = async (req, res, next) => {
   const users = await myDataSource.getRepository(User).find()
-  res.status(200).json({ users })
+  res.status(200).json(users)
 }
 
 // get
@@ -36,6 +32,8 @@ export const updateUserById: RequestHandler = async (req, res, next) => {
     myDataSource.getRepository(User).merge(user, req.body)
     const results = await myDataSource.getRepository(User).save(user)
     res.status(200).json(results)
+  } else {
+    res.status(500).json({ message: 'User not found' })
   }
 }
 
@@ -44,27 +42,3 @@ export const deleteUserById: RequestHandler = async (req, res, next) => {
   const results = await myDataSource.getRepository(User).delete(+req.params.id)
   res.status(200).json(results)
 }
-
-// export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
-//   const id = req.params.id
-//   const todoIndex = TODOS.findIndex(todo => todo.id === id)
-//   if (todoIndex < 0) {
-//     throw new Error('Could not find todo!')
-//   }
-//   const deletedTodo = TODOS[todoIndex]
-//   TODOS.splice(todoIndex, 1)
-//   res.status(200).json({ message: 'Todo updated!', deletedTodo })
-// }
-
-// export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
-//   const id = req.params.id
-//   const text = (req.body as { text: string }).text
-//   const todoIndex = TODOS.findIndex(todo => todo.id === id)
-//   if (todoIndex < 0) {
-//     throw new Error('Could not find todo!')
-//   }
-//   TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, text)
-//   res
-//     .status(200)
-//     .json({ message: 'Todo updated!', updatedTodo: TODOS[todoIndex] })
-// }
